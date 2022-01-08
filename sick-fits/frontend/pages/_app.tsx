e5e0301c79d798f69f1c0eadf7a894f89432f8c3
András Szesztai <andras.szesztai@nielsen.com>
2022-01-08 12:34:24 +0100

    feat(routerChange): add nprogress bar to visualize route change

--- sick-fits/frontend/pages/_app.tsx
+++ sick-fits/frontend/pages/_app.tsx
@@ -1,7 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { AppProps } from 'next/App';
+import NProgress from 'nprogress';
+import Router from 'next/Router';
+
+import '../components/styles/nprogress.css';
 
 import Page from '../components/Page';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Page>
