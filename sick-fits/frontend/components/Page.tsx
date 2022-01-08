459e5290b773e38e1f4f8c2aa4a5ff706280c92a
András Szesztai <andras.szesztai@nielsen.com>
2022-01-08 11:27:33 +0100

    feat(header): add initial Header and Nav components to Page

--- sick-fits/frontend/components/Page.tsx
+++ sick-fits/frontend/components/Page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './Header';
 
 interface IPageProps {
   children: React.ReactNode;
@@ -6,6 +7,7 @@ interface IPageProps {
 
 const Page: React.FC<IPageProps> = ({ children }) => (
   <div>
+    <Header />
     <h2>Page component</h2>
     {children}
   </div>
