ccc8e5fa6959149ae1942dab17c710c247fb82a1
András Szesztai <andras.szesztai@nielsen.com>
2022-01-08 17:52:48 +0100

    feat(frontend): Apollo client setup

---
+++ sick-fits/frontend/pages/_app.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
+
+import withData from '../lib/withData';
+
+import '../components/styles/nprogress.css';
+
+import Page from '../components/Page';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+// TODO: setup with typescript
+const App = ({ Component, pageProps, apollo }) => {
+  console.log(apollo);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+};
+
+App.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(App);
